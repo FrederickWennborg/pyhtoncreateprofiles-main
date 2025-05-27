416e31b7686ea36a3898de2ceaa597586347546a
AndreMartinT <torrebt.9411@gmail.com>
2025-05-27 11:28:19 +0200

    Dummy test publish branch first attempt

---
+++ dummytest.py
@@ -0,0 +1,8 @@
+import unittest
+
+class DummyAndre(unittest.TestCase):
+    def This_test_is_to_check_if_I_am_poor_or_not(self):
+        self.assertTrue(self)
+
+if __name__ == '__main__':
+    unittest.main()       
