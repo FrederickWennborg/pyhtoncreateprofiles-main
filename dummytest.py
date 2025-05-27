66b1eaa0ad2107938b0e8207530659ee34d3dfcc
AndreMartinT <torrebt.9411@gmail.com>
2025-05-27 11:55:38 +0200

    Making it back so it passes

--- dummytest.py
+++ dummytest.py
@@ -2,7 +2,7 @@ import unittest
 
 class DummyAndre(unittest.TestCase):
     def test_if_I_am_poor_or_not(self):
-        self.assertTrue(False)
+        self.assertTrue(True)
 
 if __name__ == '__main__':
     unittest.main()
