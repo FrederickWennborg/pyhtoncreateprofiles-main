f8099539e3cabec068099b22050526c7edd86206
AndreMartinT <torrebt.9411@gmail.com>
2025-05-27 11:46:12 +0200

    5th and hopefully last test that should work

--- dummytest.py
+++ dummytest.py
@@ -1,8 +1,8 @@
 import unittest
 
 class DummyAndre(unittest.TestCase):
-    def This_test_is_to_check_if_I_am_poor_or_not(self):
+    def test_if_I_am_poor_or_not(self):
         self.assertTrue(True)
 
 if __name__ == '__main__':
-    unittest.main()       
+    unittest.main()
